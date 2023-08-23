39a5bb3b0a8c9fe2b1fb3db15a411422ace8239a
rusofen <53075225+rusofen@users.noreply.github.com>
2023-08-23 15:34:39 +0300

    Add files via upload

---
+++ send2tg.php
@@ -0,0 +1,23 @@
+<?php
+define('TELEGRAM_TOKEN', '6267842917:AAEB3HA775wumPUFkjFhiMmW9Mfk7HKQeys');
+define('TELEGRAM_CHATID', '-1001580670246');
+
+
+function message_to_telegram($text)
+{
+    $ch = curl_init();
+    curl_setopt_array(
+        $ch,
+        array(
+            CURLOPT_URL => 'https://api.telegram.org/bot' . TELEGRAM_TOKEN . '/sendMessage',
+            CURLOPT_POST => TRUE,
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_TIMEOUT => 10,
+            CURLOPT_POSTFIELDS => array(
+                'chat_id' => TELEGRAM_CHATID,
+                'text' => $text,
+            ),
+        )
+    );
+    curl_exec($ch);
+}
